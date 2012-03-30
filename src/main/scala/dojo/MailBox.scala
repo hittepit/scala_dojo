ccc260283f5364e432802a8a06e16b72ed907dfb
fabrice.claes <fabrice.claes@ucm.be>
2012-03-30 09:14:04 +0200

    Solved

--- src/main/scala/dojo/MailBox.scala
+++ src/main/scala/dojo/MailBox.scala
@@ -3,9 +3,15 @@ package dojo
 class MailBox {
 
   def handleMsg(msg: PingPongMessage) = msg match{
+    case Ping => Pong
+    case Pong => Ping
     case _ => None
   }
 
-  def receive(msg: Any) = None
+  def receive(msg: Any) = msg match{
+    case message:PingPongMessage => handleMsg(message)
+    case "stop" => "stopped"
+    case ("echo",message:Any) => message
+  }
 
 }
